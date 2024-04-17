19c7a73e40f76c475b063998bacfb55393f2b907
den-zl <lashindeniskb232@gmail.com>
2024-04-17 20:10:09 +0300

    append access functions

--- libs/data_structures/exceptions/exceptions.h
+++ libs/data_structures/exceptions/exceptions.h
@@ -1,16 +1,14 @@
 #ifndef LAB_14_EXCEPTIONS_H
 #define LAB_14_EXCEPTIONS_H
 
+
 #include "/Users/denzl/CLionProjects/2sem/19.14/lab_14/libs/data_structures/vector/vector.h"
 
 // ошибка нехватки памяти
 void exceptBadAlloc(vector v);
 
 // ошибка ненайденного элемента
-void exceptIndexError(vector v, const size_t index);
-
-// ошибка - вектор пустой
-void exceptEmptyVector(vector v);
+void exceptIndexError(vector v);
 
 
 #endif //LAB_14_EXCEPTIONS_H
